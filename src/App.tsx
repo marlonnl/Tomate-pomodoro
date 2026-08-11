ebcdeb84f8d8ca3ec1f7844247c8f70210350953
Marlonn Locatelli <marlonn@gmail.com>
2026-08-11 17:59:43 -0300

    🗑️ code cleaning: deleted unused imports and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Logo } from './components/Logo'
-import { Header } from './components/Header'
 import { Container } from './components/Container'
-
-import { TimerIcon } from 'lucide-react'
 
 import './styles/theme.css'
 import './styles/global.css'
+import { Menu } from './components/Menu'
 
 export function App() {
   return (
@@ -13,21 +11,9 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
-        <Header>
-          Título
-          <button>
-            <TimerIcon />
-          </button>
-        </Header>
-      </Container>
-      <Container>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
-          tempore quibusdam reprehenderit esse a nam, nulla provident ullam
-          reiciendis recusandae voluptatum voluptate, perspiciatis ut ipsum
-          nobis consequuntur veniam. Aperiam, pariatur?
-        </p>
+        <Menu />
       </Container>
     </div>
   )
