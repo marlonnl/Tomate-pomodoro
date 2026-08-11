20780514b25da58175d65d3f00788409980106f8
Marlonn Locatelli <marlonn@gmail.com>
2026-08-11 15:17:43 -0300

    ✨ feat: added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
+import { Logo } from './components/Logo/Logo'
 import { Header } from './components/Header/Header'
+
+import { TimerIcon } from 'lucide-react'
 
 import './styles/theme.css'
 import './styles/global.css'
+import { Container } from './components/Container/Container'
 
 export function App() {
   return (
-    <div>
-      <Header>Título</Header>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
-        tempore quibusdam reprehenderit esse a nam, nulla provident ullam
-        reiciendis recusandae voluptatum voluptate, perspiciatis ut ipsum nobis
-        consequuntur veniam. Aperiam, pariatur?
-      </p>
+    <div className="container-fluid">
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Header>
+          Título
+          <button>
+            <TimerIcon />
+          </button>
+        </Header>
+      </Container>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+          tempore quibusdam reprehenderit esse a nam, nulla provident ullam
+          reiciendis recusandae voluptatum voluptate, perspiciatis ut ipsum
+          nobis consequuntur veniam. Aperiam, pariatur?
+        </p>
+      </Container>
     </div>
   )
 }
