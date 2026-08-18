590c4945064ba6506903c4127c2d513dafe04d36
Marlonn Locatelli <marlonn@gmail.com>
2026-08-18 20:13:40 -0300

    ✨ feat: added theme change logic

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
   HomeIcon,
   MoonIcon,
@@ -8,23 +9,59 @@ import {
 
 import styles from './styles.module.css'
 
+type ThemeType = 'dark' | 'light'
+
 export function Menu() {
+  const ThemeChange = {
+    dark: {
+      icon: <SunMediumIcon />,
+      name: 'claro',
+    },
+    light: {
+      icon: <MoonIcon />,
+      name: 'escuro',
+    },
+  }
+
+  const [theme, setTheme] = useState<ThemeType>(() => {
+    const storedTheme = (localStorage.getItem('theme') as ThemeType) || 'dark'
+    return storedTheme
+  })
+
+  function handleThemeChange(
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    e.preventDefault()
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark'
+      return nextTheme
+    })
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme) // guardar o tema no localStorage
+  }, [theme])
+
   return (
     <nav className={styles.navButtons}>
-      <a href="#">
+      <a href="#" title="Página inicial" aria-label="Ir para página incial">
         <HomeIcon />
       </a>
-      <a href="#">
+      <a href="#" title="Histórico" aria-label="Ver histórico">
         <TimerResetIcon />
       </a>
-      <a href="#">
+      <a href="#" title="Configurações" aria-label="Configurar app">
         <Settings2Icon />
       </a>
-      <a href="#">
-        <SunMediumIcon />
-      </a>
-      <a href="#">
-        <MoonIcon />
+      <a
+        href="#"
+        title={`Mudar para tema ${ThemeChange[theme]['name']}`}
+        aria-label={`Alterar tema para ${ThemeChange[theme]['name']}`}
+        onClick={e => handleThemeChange(e)}
+      >
+        {ThemeChange[theme]['icon']}
       </a>
     </nav>
   )
