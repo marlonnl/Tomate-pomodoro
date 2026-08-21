200291e7ef1cff23d49b463f2460d49a37996611
Marlonn Locatelli <marlonn@gmail.com>
2026-08-21 15:41:38 -0300

    🚧 feat: starting a new task

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,13 +1,63 @@
-import { Play, TimerOff } from 'lucide-react'
+import { useRef } from 'react'
+
 import { Button } from '../Button'
 import { Input } from '../Input'
 
+import { Play, TimerOff } from 'lucide-react'
 import styles from './styles.module.css'
+import type { TaskModel } from '../../models/TaskModel'
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
+import { getNextCycle } from '../../utils/getNextCycle'
 
 export function Form() {
+  const { state, setState } = useTaskContext()
+  const taskNameInput = useRef<HTMLInputElement>(null)
+
+  const nexCycle = getNextCycle(state.currentCycle)
+
+  function handleCreateNewTask(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    // if (taskNameInput.current === null) return
+    // const taskName = taskNameInput.current.value.trim()
+    const taskNameInputValue = taskNameInput.current?.value.trim()
+
+    const taskName: string = taskNameInputValue
+      ? taskNameInputValue
+      : 'Pomodoro task'
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'work',
+    }
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: nexCycle,
+        secondsRemaining,
+        formattedSecondsReamining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      }
+    })
+  }
+
   return (
-    <form className="form" action="">
-      <Input type="text" id="timerName" placeholder="nome da tarefa" />
+    <form onSubmit={e => handleCreateNewTask(e)} className="form" action="">
+      <Input
+        type="text"
+        id="taskName"
+        placeholder="nome da tarefa"
+        ref={taskNameInput}
+      />
       <div className={styles.buttonControls}>
         <Button>
           <Play />
