db420c04ab09ec2ede51716ecb474d0334bea224
Marlonn Locatelli <marlonn@gmail.com>
2026-08-28 19:15:49 -0300

    ✨ feat: added contdown and complete task actions

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -1,9 +1,12 @@
 import type { TaskModel } from '../../models/TaskModel'
+import type { TaskStateModel } from '../../models/TaskStateModel'
 
 export enum TaskActionTypes {
   START_TASK = 'START_TASK',
   INTERRUPT_TASK = 'INTERRUPT_TASK',
   RESET_STATE = 'RESET_STATE',
+  COUNTDOWN = 'COUNTDOWN',
+  COMPLETE_TASK = 'COMPLETE',
 }
 
 export type TaskActionModel =
@@ -16,4 +19,12 @@ export type TaskActionModel =
     }
   | {
       type: TaskActionTypes.RESET_STATE
+    }
+  | {
+      type: TaskActionTypes.COUNTDOWN
+      // payload: { 'secondsRemaining': number }
+      payload: Pick<TaskStateModel, 'secondsRemaining'>
+    }
+  | {
+      type: TaskActionTypes.COMPLETE_TASK
     }
