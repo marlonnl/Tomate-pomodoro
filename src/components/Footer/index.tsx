a34bbf449f8eddaa1b49e86ccc798bc4e9d59896
Marlonn Locatelli <marlonn@gmail.com>
2026-09-02 22:05:44 -0300

    ♻️ about project link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,8 +4,8 @@ import styles from './styles.module.css'
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <RouterLink href="/about-pomodoro">sobre o método pomodoro</RouterLink>
-      <RouterLink href="/">
+      <RouterLink href="/about-pomodoro">Método pomodoro</RouterLink>
+      <RouterLink href="/about-project">
         Tomate app &copy; {new Date().getFullYear()}
       </RouterLink>
     </footer>
